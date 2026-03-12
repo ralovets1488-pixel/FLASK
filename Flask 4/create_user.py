3bbd4379655ed01ca66bde5494d88a42715aab8b
ralovets1488-pixel <ralovets1488@gmail.com>
2026-03-12 15:38:19 +0700

    Delete Flask 4 directory

--- Flask 4/create_user.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from models import User
-
-with app.app_context():
-    user = User(username='admin')
-    user.set_password('1234')
-    db.session.add(user)
-    db.session.commit()
-    print("Пользователь создан.")
